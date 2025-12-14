55e80659a34353b75e7d2db9453b78d43bde4bd5
Waqas Khan <waqasrasheed413@gmail.com>
2025-12-14 15:17:04 +0000

    Add .gitignore and integrate Mochawesome HTML reporting for Cypress tests

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,14 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // No custom events yet
     },
+  },
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: false,
+    json: true,
   },
 });
